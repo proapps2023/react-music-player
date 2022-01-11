e4dcdbce9c7347d245f0722ff06a66c13ac58eb3
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-01-11 10:42:45 +0400

    update time and duration on the current song

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,7 @@
 import React, {useRef} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react/cjs/react.development";
 
 
 const Player = ({currentSong, isPlaying, setIsPlaying}) => {
@@ -18,20 +19,43 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
             setIsPlaying(!isPlaying);
         }
     };
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo, currentTime:current, duration:duration});
 
-    return(
+    };
+
+    const getTime = (time) => {
+        return (
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+          );
+    };
+    //state
+    const [songInfo, setSongInfo] = useState ({
+        currentTime: null,
+        duration: null,
+    });
+
+
+    return (
     <div className="player">
         <div className="time-control">
-            <p>Start Time</p>
+            <p>{getTime(songInfo.currentTime)}</p>
             <input type="range"/>
-            <p>End Time</p>
+            <p>{getTime(songInfo.duration)}</p>
         </div>
         <div className="play-control">
             <FontAwesomeIcon className="skib-back" size="2x" icon={faAngleLeft} />    
             <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
             <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
         </div>
-        <audio ref={audioRef} src={currentSong.audio}></audio>
+        <audio 
+        onTimeUpdate={timeUpdateHandler} 
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef} 
+        src={currentSong.audio}>
+        </audio>
     </div>
     ) 
 };
