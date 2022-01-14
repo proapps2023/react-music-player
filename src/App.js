53cf5e764d988f7ce00a32550311a21dcb30330a
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-01-15 03:09:41 +0400

    toggle library

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import data from "./util";
 import Player from "./components/Player";
 import Song from "./components/Songs";
 import Library from "./components/Library";
+import Nav from "./components/Nav";
 
 function App() {
 	//ref
@@ -21,6 +22,7 @@ function App() {
 		currentTime: 0,
 		duration: 0,
 	});
+	const [libraryStatus, setLibraryStatus] = useState(false);
 	const timeUpdateHandler = (e) => {
         const current = e.target.currentTime;
         const duration = e.target.duration;
@@ -31,6 +33,7 @@ function App() {
 
     return (
     	<div className="App">
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
     	    <Song currentSong={currentSong} />
     	    <Player 
 				audioRef ={audioRef}
@@ -46,6 +49,7 @@ function App() {
 				setCurrentSong={setCurrentSong}
 				isPlaying={isPlaying}
 				setSongs={setSongs}
+				libraryStatus={libraryStatus}
 		    />
 			<audio 
             	onTimeUpdate={timeUpdateHandler} 
